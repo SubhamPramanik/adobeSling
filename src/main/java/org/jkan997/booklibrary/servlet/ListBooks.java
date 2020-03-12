fbacfba763b94b2f3134c1c8d0749eb5bbcc1183
Subham Pramanik <subhampramanik@gmail.com>
2020-03-13 00:15:45 +0530

    ListBook working, To add search

--- src/main/java/org/jkan997/booklibrary/servlet/ListBooks.java
+++ src/main/java/org/jkan997/booklibrary/servlet/ListBooks.java
@@ -1,6 +1,13 @@
 package org.jkan997.booklibrary.servlet;
 
+import com.google.gson.stream.JsonWriter;
+import java.io.PrintWriter;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Session;
 import javax.servlet.Servlet;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Activate;
@@ -33,6 +40,44 @@ public class ListBooks extends SlingSafeMethodsServlet {
         /*
         YOUR IMPLEMENTATION HERE
          */
+        
+        try {
+            
+            response.setContentType("application/json");
+            PrintWriter wrt = response.getWriter();
+            Session jcrSession = repository.loginAdministrative(null);
+            Node rootNode = jcrSession.getRootNode();
+            Node booksNode = rootNode.getNode("books");
+            Node fictionNode = booksNode.getNode("fiction");
+            Node nonFictionNode = booksNode.getNode("nonfiction");
+            
+            NodeIterator fictionBooks = fictionNode.getNodes();
+            NodeIterator nonFictionBooks = nonFictionNode.getNodes();
+            
+            JsonWriter writer = new JsonWriter(wrt);
+            writer.beginArray();
+            while(fictionBooks.hasNext()) {
+                Node childBook = fictionBooks.nextNode();
+                writer.beginObject();
+                writer.name("title").value(childBook.getProperty("title").getString());
+                writer.name("author").value(childBook.getProperty("author").getString());
+                writer.name("genre").value(childBook.getParent().getName());
+                writer.name("path").value(childBook.getPath());
+                writer.endObject();
+            }
+            jcrSession.save();
+            writer.endArray();
+            writer.close();
+            wrt.close();
+            
+            
+            
+            
+            
+        } catch (Exception ex) {
+            throw new RuntimeException ("Following Exception tok place: " + ex); 
+        }
+        
         throw new RuntimeException("" + getClass().getCanonicalName() + " not implemented yet.");
     }
 
