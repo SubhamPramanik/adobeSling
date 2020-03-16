4638e9591a0b87b722d5efc404e6f55b09693595
Subham Pramanik <subhampramanik@gmail.com>
2020-03-16 12:09:37 +0530

    BookListModel and booklist.html completed

--- src/main/java/org/jkan997/booklibrary/htl/BooklistModel.java
+++ src/main/java/org/jkan997/booklibrary/htl/BooklistModel.java
@@ -1,13 +1,28 @@
 package org.jkan997.booklibrary.htl;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.jcr.query.Query;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.slf4j.LoggerFactory;
 
 @Model(adaptables = SlingHttpServletRequest.class,
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class BooklistModel {
+    
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(BooklistModel.class);
+    
+    @SlingObject
+    private SlingHttpServletRequest request;
+    
+    private List<Book> book = new ArrayList<Book>();
     
     public String getDate() {
         return (new Date()).toString();
@@ -16,4 +31,30 @@ public class BooklistModel {
     /*
         YOUR HTL MODEL HERE HERE
      */
+    
+    public List<Book> getBook() {
+        LOGGER.info("getBook called");
+        return book;
+    }
+    
+    @PostConstruct
+    protected void init() {
+        LOGGER.info("Inside PC called");
+        try {
+            String QUERY = "SELECT * FROM [nt:unstructured] WHERE ISDESCENDANTNODE ([/books])";
+            Iterator<Resource> result = request.getResourceResolver().findResources(QUERY, Query.JCR_SQL2);
+            LOGGER.info("Inside PostConstruct");
+            
+            while(result.hasNext()) {
+                Book bookItem = result.next().adaptTo(Book.class);
+                if (book != null)
+                    LOGGER.info(bookItem.getTitle());
+                    book.add(bookItem);
+            }
+        } catch (Exception ex) {
+            LOGGER.error("Exception caused: " + ex);
+        }
+        
+    }
+    
 }
